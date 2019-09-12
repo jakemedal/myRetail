93bffde2975d47cfbd0b7e649899804d9ff968c1
jake.medal <jake.medal@thomsonreuters.com>
2019-09-12 11:46:36 -0500

    Remove logging declaration and move to annotation

--- src/main/java/com/myRetail/repository/MongoDBProductRepository.java
+++ src/main/java/com/myRetail/repository/MongoDBProductRepository.java
@@ -1,24 +1,21 @@
 package com.myRetail.repository;
 
 import com.myRetail.repository.exception.ProductPriceNotFoundException;
-import org.apache.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.Assert;
 
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
+@Log4j2
 @Repository
 public class MongoDBProductRepository implements ProductRepository {
-    private static final Logger LOG = Logger.getLogger(MongoDBProductRepository.class);
-
     private final MongoOperations operations;
 
     @Autowired
     public MongoDBProductRepository(MongoOperations operations) {
-        Assert.notNull(operations, "MongoOperations cannot be null.");
         this.operations = operations;
     }
 
@@ -34,7 +31,6 @@ public class MongoDBProductRepository implements ProductRepository {
             throw new ProductPriceNotFoundException("No product price information found for ID: " + id);
         }
 
-        LOG.info("Returning productPriceDto=" + productPriceDTO);
         return productPriceDTO;
     }
 }
